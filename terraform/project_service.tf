241a8ac4c8ca5c24639e759ead64698b8d719089
ishihaya <ishidahayate49@gmail.com>
2021-07-16 10:12:45 +0900

    enable service2

--- terraform/project_service.tf
+++ terraform/project_service.tf
@@ -1,3 +1,17 @@
+resource "google_project_service" "service_usage" {
+  project = var.project_name
+  service = "serviceusage.googleapis.com"
+  disable_dependent_services = true
+}
+
+resource "google_project_service" "iam" {
+  project = var.project_name
+  service = "iam.googleapis.com"
+  disable_dependent_services = true
+}
+
 resource "google_project_service" "gke" {
   project = var.project_name
+  service = "container.googleapis.com"
+  disable_dependent_services = true
 }
