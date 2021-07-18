39ded728ae5d25c1bcf621bd1dd50e252b468f91
ishihaya <ishidahayate49@gmail.com>
2021-07-18 22:33:19 +0900

    add cloud run

--- terraform/piped.tf
+++ terraform/piped.tf
@@ -24,3 +24,42 @@ resource "google_project_iam_member" "piped" {
   role    = "projects/${var.project_name}/roles/${google_project_iam_custom_role.piped.role_id}"
   member  = "serviceAccount:${google_service_account.piped.email}"
 }
+
+resource "google_cloud_run_service" "piped" {
+  name                       = "piped"
+  project                    = var.project_name
+  location                   = var.project_region
+  autogenerate_revision_name = false
+  template {
+    metadata {
+      name = "piped"
+      annotations = {
+        "autoscaling.knative.dev/maxScale"  = "1" # This must be 1
+        "autoscaling.knative.dev/minScale"  = "1" # This must be 1
+        "run.googleapis.com/ingress"        = "internal"
+        "run.googleapis.com/ingress-status" = "internal"
+      }
+    }
+    spec {
+      container_concurrency = 1 # This must be 1
+      containers {
+        image = "gcr.io/pipecd/piped:v0.12.0"
+        args = [
+          "piped",
+          "--metrics=true",
+          # TODO: secretを作成してconfigを読み込むようにする
+          # "--config-file=/etc/piped-config/config.yaml",
+        ]
+        ports {
+          container_port = 9085
+        }
+        resources {
+          limits = {
+            "cpu"    = "1000m"
+            "memory" = "512Mi"
+          }
+        }
+      }
+    }
+  }
+}
