0234a71407b8230bd5deae6f488dc17511d6a256
ishihaya <ishidahayate49@gmail.com>
2021-07-18 15:42:30 +0900

    del gke

--- terraform/project_service.tf
+++ terraform/project_service.tf
@@ -1,17 +1,11 @@
 resource "google_project_service" "service_usage" {
-  project = var.project_name
-  service = "serviceusage.googleapis.com"
+  project                    = var.project_name
+  service                    = "serviceusage.googleapis.com"
   disable_dependent_services = true
 }
 
 resource "google_project_service" "iam" {
-  project = var.project_name
-  service = "iam.googleapis.com"
-  disable_dependent_services = true
-}
-
-resource "google_project_service" "kubernetes" {
-  project = var.project_name
-  service = "container.googleapis.com"
+  project                    = var.project_name
+  service                    = "iam.googleapis.com"
   disable_dependent_services = true
 }
