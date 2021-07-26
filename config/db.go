873652d5cb0bf75c0123c02811ece33ea2e737c6
ishihaya <ishidahayate49@gmail.com>
2021-07-26 22:01:29 +0900

    add database in app

---
+++ config/db.go
@@ -0,0 +1,24 @@
+package config
+
+import (
+	"fmt"
+	"os"
+)
+
+func DSN() (dsn string) {
+	user := os.Getenv("MYSQL_USER")
+	password := os.Getenv("MYSQL_PASSWORD")
+	dbname := os.Getenv("MYSQL_DATABASE")
+	host := os.Getenv("DB_HOST")
+	if host != "" {
+		// tcp
+		port := os.Getenv("DB_PORT")
+		dsn = fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=true", user, password, host, port, dbname)
+		return
+	}
+
+	// unix
+	dbSocketPath := os.Getenv("DB_SOCKET_PATH")
+	dsn = fmt.Sprintf("%s:%s@unix(%s)/%s?parseTime=true", user, password, dbSocketPath, dbname)
+	return
+}
--- main.go
+++ main.go
@@ -1,14 +1,22 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
 
+	// to connect mysql db
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/ishihaya/company-official-app-backend/config"
 )
 
 func main() {
+	dsn := config.DSN()
+	_, err := sql.Open("mysql", dsn)
+	if err != nil {
+		log.Fatalf("failed to connect to database: %+v", err)
+	}
 	http.Handle("/", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
 		fmt.Fprintf(w, "Hello World")
