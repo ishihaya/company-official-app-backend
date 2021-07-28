1caee4e6d5d360c15f2e7d6c849f4e9d679ca1e8
ishihaya <ishidahayate49@gmail.com>
2021-07-28 21:50:24 +0900

    fix

--- infra/logger/logger.go
+++ infra/logger/logger.go
@@ -1,6 +1,10 @@
 package logger
 
-import "go.uber.org/zap"
+import (
+	"sync"
+
+	"go.uber.org/zap"
+)
 
 type logging interface {
 	Debug(msg string, field ...zap.Field)
@@ -16,3 +20,65 @@ type logging interface {
 }
 
 type loggingImpl struct{}
+
+var Logging logging
+var zapLogger *zap.Logger
+var sugar *zap.SugaredLogger
+var once sync.Once
+
+func (log *loggingImpl) Debug(msg string, field ...zap.Field) {
+	zapLogger.Debug(msg, field...)
+}
+
+func (log *loggingImpl) Error(msg string, field ...zap.Field) {
+	zapLogger.Error(msg, field...)
+}
+
+func (log *loggingImpl) Info(msg string, field ...zap.Field) {
+	zapLogger.Info(msg, field...)
+}
+
+func (log *loggingImpl) Warn(msg string, field ...zap.Field) {
+	zapLogger.Warn(msg, field...)
+}
+
+func (log *loggingImpl) Fatal(msg string, field ...zap.Field) {
+	zapLogger.Fatal(msg, field...)
+}
+
+func (log *loggingImpl) Debugf(template string, args ...interface{}) {
+	defer func() {
+		_ = sugar.Sync()
+	}()
+	sugar.Debugf(template, args)
+}
+
+func (log *loggingImpl) Errorf(template string, args ...interface{}) {
+	defer func() {
+		_ = sugar.Sync()
+	}()
+	sugar.Errorf(template, args)
+}
+
+func (log *loggingImpl) Infof(template string, args ...interface{}) {
+	defer func() {
+		_ = sugar.Sync()
+	}()
+	sugar.Infof(template, args)
+}
+
+func (log *loggingImpl) Warnf(template string, args ...interface{}) {
+	defer func() {
+		_ = sugar.Sync()
+	}()
+	sugar.Warnf(template, args)
+}
+
+func (log *loggingImpl) Fatalf(template string, args ...interface{}) {
+	defer func() {
+		_ = sugar.Sync()
+	}()
+	sugar.Fatalf(template, args)
+}
+
+func New() {}
