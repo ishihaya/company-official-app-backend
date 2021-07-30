af7cfc62b3e54ecb658e311781a4f3e75d35cf66
ishihaya <ishidahayate49@gmail.com>
2021-07-30 10:18:28 +0900

    add function in repository

--- interface/dataaccess/repository/user.go
+++ interface/dataaccess/repository/user.go
@@ -1,3 +1,22 @@
 package repository
 
-type userRepository struct{}
+import (
+	"github.com/ishihaya/company-official-app-backend/domain/entity"
+	"github.com/ishihaya/company-official-app-backend/domain/repository"
+	"github.com/ishihaya/company-official-app-backend/infra/db"
+)
+
+type userRepository struct {
+	conn *db.Conn
+}
+
+func NewUserRepository(conn *db.Conn) repository.UserRepository {
+	return &userRepository{
+		conn: conn,
+	}
+}
+
+func (u *userRepository) GetByAuthID(authID string) (*entity.User, error) {
+	// TODO
+	return nil, nil
+}
