5b4ab88b1b5bb03b897f261f62c845eb263270c3
ishihaya <ishidahayate49@gmail.com>
2021-08-01 15:43:19 +0900

    add operator

--- interface/operator/app_id.go
+++ interface/operator/app_id.go
@@ -1,10 +1,12 @@
 package operator
 
 import (
+	"math/rand"
 	"time"
 
 	"github.com/ishihaya/company-official-app-backend/domain/entity"
 	"github.com/ishihaya/company-official-app-backend/domain/operator"
+	"github.com/oklog/ulid/v2"
 )
 
 type appIDOperator struct{}
@@ -14,17 +16,12 @@ func NewAppIDOperator() operator.AppIDOperator {
 }
 
 func (a *appIDOperator) Generate(t time.Time) (entity.AppID, error) {
-	// _, err := ulidgo.Generate(t)
-	// if err != nil {
-	// 	return "", nil
-	// }
-	// var id entity.AppID = ulid
-	return "", nil
-	// timeStamp := ulid.Timestamp(t)
-	// entropy := ulid.Monotonic(rand.New(rand.NewSource(t.UnixNano())), 0)
-	// ulid, err := ulid.New(timeStamp, entropy)
-	// if err != nil {
-	// 	return "", err
-	// }
-	// return ulid.String(), nil
+	timeStamp := ulid.Timestamp(t)
+	entropy := ulid.Monotonic(rand.New(rand.NewSource(t.UnixNano())), 0)
+	ulid, err := ulid.New(timeStamp, entropy)
+	if err != nil {
+		return "", err
+	}
+	id := entity.AppID(ulid.String())
+	return id, nil
 }
