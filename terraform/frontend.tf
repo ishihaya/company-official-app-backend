1ea4bcc21f14f9fcd56eb8656c252b32f98fc8a6
ishihaya <ishidahayate49@gmail.com>
2021-08-07 16:54:04 +0900

    add service account

--- terraform/frontend.tf
+++ terraform/frontend.tf
@@ -4,5 +4,14 @@ resource "google_service_account" "firebase_hosting" {
   description  = "フロントエンドサービスをfirebase hostingでデプロイする用"
 }
 
-resource "google_project_iam_member" "firebase_hosting" {
+resource "google_project_iam_member" "firebase_hosting_admin" {
+  project = var.project_name
+  member  = "serviceAccount:${google_service_account.firebase_hosting.email}"
+  role    = "roles/firebasehosting.admin"
+}
+
+resource "google_project_iam_member" "firebase_hosting_api_keys_viewer" {
+  project = var.project_name
+  member  = "serviceAccount:${google_service_account.firebase_hosting.email}"
+  role    = "roles/serviceusage.apiKeysViewer"
 }
